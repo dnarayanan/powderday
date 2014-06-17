1. Fixed the total luminosity in SPS generation (for binned SEDs) by scaling by the total mass int he bin, not the mass associated with that bin.
2. killed the writing the coordinates grid

3. fixed the dx,dy,dz,xcent,ycent,zcent calculations in all modules

4. cleaned up pd front end a bit

5. removed boosting of stars (for now)

6. import statements in grid_construction went to the top

7. put in the metallicities of stars for both individual additions, and binned ssources.  this made metallicity bins not defined anymore as this is defined by the FSPS files in fsps/isochrones

SED_gen.py
@@ -7,6 +7,7 @@ from astropy.io import ascii
 import constants as const
 import pdb
 import sys
+from plot_generate import mass_weighted_distribution as mwd
 
 import fsps 
 from datetime import datetime
@@ -19,16 +20,17 @@ from multiprocessing import Pool
 
 
 class Stars:
-    def __init__(self,mass,metals,positions,age,sed_bin=[-1,-1,-1],lum=-1):
+    def __init__(self,mass,metals,positions,age,sed_bin=[-1,-1,-1],lum=-1,fsps_zmet=20):
         self.mass = mass
         self.metals = metals
         self.positions = positions
         self.age = age
         self.sed_bin = sed_bin
         self.lum = lum
+        self.fsps_zmet = fsps_zmet
 
     def info(self):
-        return(self.mass,self.metals,self.positions,self.age,self.sed_bin,self.lum)
+        return(self.mass,self.metals,self.positions,self.age,self.sed_bin,self.lum,self.fsps_zmet)
 
 
 
@@ -54,12 +56,23 @@ def star_list_gen(boost,xcent,ycent,zcent,dx,dy,dz):
     nstars = len(age)
     print 'number of new stars =',nstars
     
-   
+
+    #calculate the fsps interpolated metallicity
+
+    #if the metallicity has many fields, and not just global
+    #metallicity then just extract the global metallicity
+
+    if metals.ndim > 1:
+        metals = metals[:,0]
+
+    zmet = fsps_metallicity_interpolate(metals)
+    mwd(zmet,mass,'zmet_distribution.png')
+
     #create the stars_list full of Stars objects
     stars_list = []
-    for i in range(nstars):
-        stars_list.append(Stars(mass[i],metals[i],positions[i],age[i]))
 
+    for i in range(nstars):
+        stars_list.append(Stars(mass[i],metals[i],positions[i],age[i],fsps_zmet=zmet[i]))
 
 
     #boost stellar positions to grid center
@@ -202,10 +215,11 @@ def allstars_sed_gen(stars_list,diskstars_list,bulgestars_list):
     
 
     #get just the wavelength array
-    sp = fsps.StellarPopulation(tage=stars_list[0].age,imf_type=1,sfh=0)
-    spec = sp.get_spectrum(tage=stars_list[0].age)
+    sp = fsps.StellarPopulation(tage=stars_list[0].age,imf_type=1,sfh=0,zmet=stars_list[0].fsps_zmet)
+    spec = sp.get_spectrum(tage=stars_list[0].age,zmet=stars_list[0].fsps_zmet)
     nu = 1.e8*const.c/spec[0]
     nlam = len(nu)
+
 
 
 
@@ -276,13 +290,13 @@ def allstars_sed_gen(stars_list,diskstars_list,bulgestars_list):
         #deal since these SEDs don't end up getting added to the model in
         #source_creation as long as COSMOFLAG == True.  
         
-        sp = fsps.StellarPopulation(tage = cfg.par.disk_stars_age,imf_type=1,sfh=0)
-        spec = sp.get_spectrum(tage=cfg.par.disk_stars_age)
+        sp = fsps.StellarPopulation(tage = cfg.par.disk_stars_age,imf_type=1,sfh=0,zmet=20)
+        spec = sp.get_spectrum(tage=cfg.par.disk_stars_age,zmet=20)
         disk_fnu = spec[1]
         
         #calculate the SED for bulge stars
-        sp = fsps.StellarPopulation(tage = cfg.par.bulge_stars_age,imf_type=1,sfh=0)
-        spec = sp.get_spectrum(tage=cfg.par.bulge_stars_age)
+        sp = fsps.StellarPopulation(tage = cfg.par.bulge_stars_age,imf_type=1,sfh=0,zmet=20)
+        spec = sp.get_spectrum(tage=cfg.par.bulge_stars_age,zmet=20)
         bulge_fnu = spec[1]
     
 
@@ -324,8 +338,8 @@ def newstars_gen(stars_list):
     
 
     #first figure out how many wavelengths there are
-    sp = fsps.StellarPopulation(tage=stars_list[0].age,imf_type=1,sfh=0)
-    spec = sp.get_spectrum(tage=stars_list[0].age)
+    sp = fsps.StellarPopulation(tage=stars_list[0].age,imf_type=1,sfh=0,zmet=stars_list[0].fsps_zmet)
+    spec = sp.get_spectrum(tage=stars_list[0].age,zmet=stars_list[0].fsps_zmet)
     nu = 1.e8*const.c/spec[0]
     
 
@@ -348,8 +362,8 @@ def newstars_gen(stars_list):
     #calculate the SEDs for new stars
     for i in range(len(stars_list)):
         
-        sp = fsps.StellarPopulation(tage=stars_list[i].age,imf_type=1,sfh=0)
-        spec = sp.get_spectrum(tage=stars_list[i].age)
+        sp = fsps.StellarPopulation(tage=stars_list[i].age,imf_type=1,sfh=0,zmet=stars_list[i].fsps_zmet)
+        spec = sp.get_spectrum(tage=stars_list[i].age,zmet=stars_list[i].fsps_zmet)
 
         stellar_nu[:] = 1.e8*const.c/spec[0]
         stellar_fnu[i,:] = spec[1]
@@ -357,5 +371,32 @@ def newstars_gen(stars_list):
     return stellar_fnu
 
 
+
+
+
+
+def fsps_metallicity_interpolate(metals):
+
+    #takes a list of metallicities for star particles, and returns a
+    #list of interpolated metallicities
+    
+    fsps_metals = np.loadtxt(cfg.par.metallicity_legend)
+    nstars = len(metals)
+    
+    zmet = []
+
+    for i in range(nstars):
+        zmet.append(find_nearest_zmet(fsps_metals,metals[i]))
+    
+    return zmet
+    
+def find_nearest_zmet(array,value):
+    #this is modified from the normal find_nearest in that it forces
+    #the output to be 1 index higher than the true value since the
+    #minimum zmet value fsps will take is 1 (not 0)
+
+    idx = (np.abs(array-value)).argmin()
+    
+    return idx+1
 
 

grid_construction.py
@@ -8,7 +8,11 @@ from astropy.table import Table
 from astropy.io import ascii
 import hyperion_octree_stats as hos
 from gridstats import gridstats
-from octree_zoom import octree_zoom
+from octree_zoom import octree_zoom_bbox_filter
+from plot_generate import proj_plots
+from yt.mods import *
+from yt.geometry.oct_container import OctreeContainer
+from yt.geometry.selection_routines import AlwaysSelector
 
 import constants as const
 
@@ -22,10 +26,8 @@ import sys
 
 def yt_octree_generate():
     
-    from yt.mods import *
-    from yt.geometry.oct_container import OctreeContainer
-    from yt.geometry.selection_routines import AlwaysSelector
-
+   
+    
 
     fname = cfg.par.hydro_dir+cfg.par.Gadget_snap_name
 
@@ -60,18 +62,16 @@ def yt_octree_generate():
                     np.absolute(min(y)),np.absolute(max(y)),
                     np.absolute(min(z)),np.absolute(max(z))])
     
-    #the bounding box needs to be a good bit bigger than the actual
-    #min/maxes of the particle locations for constructing the octree
- #   bbox = [[min(x)*2,max(x)*2],
- #           [min(y)*2,max(y)*2],
- #           [min(z)*2,max(z)*2]]
-
-    bbox = [[-2*bbox_lim,2*bbox_lim],
-            [-2*bbox_lim,2*bbox_lim],
-            [-2*bbox_lim,2*bbox_lim]]
+    
+    print '[grid_construction]: bbox_lim = ',bbox_lim
+    bbox = [[-2.*bbox_lim,2.*bbox_lim],
+            [-2.*bbox_lim,2.*bbox_lim],
+            [-2.*bbox_lim,2.*bbox_lim]]
              
     
-
+ 
+             
+    
 
     unit_base = {'UnitLength_in_cm'         : 3.08568e+21,
                  'UnitMass_in_g'            :   1.989e+43,
@@ -85,27 +85,24 @@ def yt_octree_generate():
     
         pf = load(fname,unit_base=unit_base,bounding_box=bbox,over_refine_factor=cfg.par.oref,n_ref=cfg.par.n_ref)
 
+    
     else:
     
-        pf = octree_zoom(fname,unit_base,bbox)
+        pf = octree_zoom_bbox_filter(fname,unit_base,bbox)
 
 
     pf.index
 
+  
 
 
-    
+
+   
     #---------------------------------------------------------------
     #PLOTTING DIAGNOSTIC PROJECTION PLOTS
     #---------------------------------------------------------------
-    print '\n[grid_construction] Saving Diagnostic Projection Plots \n'
-    p = ProjectionPlot(pf,"z",("deposit","PartType0_smoothed_density"))
-    p.save()
-    p = ProjectionPlot(pf,"x",("deposit","PartType0_smoothed_density"))
-    p.save()
-    p = ProjectionPlot(pf,"y",("deposit","PartType0_smoothed_density"))
-    p.save()
- 
+    
+    #proj_plots(pf)
 
 
 
@@ -115,7 +112,7 @@ def yt_octree_generate():
     from yt.data_objects.particle_unions import ParticleUnion
     pu = ParticleUnion("all", list(pf.particle_types_raw))
     
-    saved = pf.index.oct_handler.save_octree(always_descend=True)
+    saved = pf.index.oct_handler.save_octree()
  
     always = AlwaysSelector(None)
     ir1 = pf.index.oct_handler.ires(always)  #refinement levels
@@ -123,7 +120,7 @@ def yt_octree_generate():
     fw1 = pf.index.oct_handler.fwidth(always)  #width of cell in kpc
     
 
-
+    
 
     print '----------------------------'
     print 'yt Octree Construction Stats'
@@ -154,6 +151,7 @@ def yt_octree_generate():
     refined = refined2
 
 
+
     #smooth the data on to the octree
     
     volume = np.zeros(len(refined))
@@ -164,45 +162,61 @@ def yt_octree_generate():
 
    
 
- 
-
-    mass_grid = np.zeros(len(wFalse))
-
-
-    if cfg.par.CONSTANT_DUST_GRID == False: #this is the default; if True is set, then we'll 
-
+    if cfg.par.CONSTANT_DUST_GRID == False: 
 
  
         '''USING YT SMOOTHING'''
 
-
-        from particle_smooth_yt import yt_smooth
-        metallicity_smoothed,density_smoothed = yt_smooth(pf)
-        dust_smoothed = np.zeros(len(refined))
         
-
-
+        from particle_smooth_yt import yt_smooth
+        metallicity_smoothed,density_smoothed,masses_smoothed = yt_smooth(pf)
 
         
         '''
-        dust_smoothed[wFalse] = mass_smoothed * metallicity_smoothed * cfg.par.dusttometals_ratio
+        #DEBUG 061314
+        wm = np.where(metallicity_smoothed > 0)[0]
+        print np.median(metallicity_smoothed[wm])
+        wm2 = np.where(metallicity_smoothed > (np.min(metallicity_smoothed[wm]*2.)))
+        print np.median(metallicity_smoothed[wm2])
+        wmless = np.where(metallicity_smoothed < np.min(metallicity_smoothed[wm2]))[0]
+        metallicity_smoothed[wmless] = np.median(metallicity_smoothed[wm2])
+        print np.median(metallicity_smoothed)
+
+
+        wd = np.where(density_smoothed > 0)[0]
+        print np.median(density_smoothed[wd])
+        wd2 = np.where(density_smoothed > (np.min(density_smoothed[wd]*200.)))
+        print np.median(density_smoothed[wd2])
+        wdless = np.where(density_smoothed < np.min(density_smoothed[wd2]))[0]
+        density_smoothed[wdless] = np.median(density_smoothed[wd2])
+        print np.median(density_smoothed)
+
+        '''
+
+
+
+
+
+        dust_smoothed = np.zeros(len(refined))
+        
+        print '[grid_construction: ] len(wFalse) = ',len(wFalse)
+        print '[grid_construction: ] len(metallicity_smoothed) = ',len(metallicity_smoothed)
+
+       
+        
+        '''
+        dust_smoothed[wFalse] = masses_smoothed * metallicity_smoothed * cfg.par.dusttometals_ratio
         dust_density_grid = dust_smoothed/volume #in gm/cm^-3       
+        dust_density_grid[wTrue] = 0
         '''
               
         dust_smoothed[wFalse] = metallicity_smoothed * density_smoothed * cfg.par.dusttometals_ratio
-        dust_density_grid = dust_smoothed #in gm/cm^-3       
-        #since volume = 0 where there's a True, dust_density_grid is nan        
-        #where there's trues, so we have to fix this                            
-        dust_density_grid[wTrue] = 0
-         
+
         
     else:
         print 'cfg.par.CONSTANT_DUST_GRID=True'
         print 'setting constant dust grid to 4.e-22'
-        dust_density_grid = np.zeros(len(refined))+4.e-27
-        #since volume = 0 where there's a True, dust_density_grid is nan
-        #where there's trues, so we have to fix this
-        dust_density_grid[wTrue] = 0
+        dust_smoothed = np.zeros(len(refined))+4.e-23
 
 
 
@@ -211,40 +225,43 @@ def yt_octree_generate():
 
      #file I/O
     print 'Writing Out the Coordinates and Logical Tables'
+   
 
-    xmin = fc1[:,0]-fw1[:,0]
-    xmax = fc1[:,0]+fw1[:,0]
-    ymin = fc1[:,1]-fw1[:,1]
-    ymax = fc1[:,1]+fw1[:,1]
-    zmin = fc1[:,2]-fw1[:,2]
-    zmax = fc1[:,2]+fw1[:,2]
+    xmin = fc1[:,0]-fw1[:,0]/2.
+    xmax = fc1[:,0]+fw1[:,0]/2.
+    ymin = fc1[:,1]-fw1[:,1]/2.
+    ymax = fc1[:,1]+fw1[:,1]/2.
+    zmin = fc1[:,2]-fw1[:,2]/2.
+    zmax = fc1[:,2]+fw1[:,2]/2.
 
 
-
+   
     xcent_orig,ycent_orig,zcent_orig,dx,dy,dz = grid_center(xmin,xmax,ymin,ymax,zmin,zmax)
     boost = np.array([xcent_orig,ycent_orig,zcent_orig])*1.e3*const.pc
 
-
-
+    '''
     xmin,xmax,ymin,ymax,zmin,zmax = grid_coordinate_boost(xmin,xmax,ymin,ymax,zmin,zmax)
+   
 
-    coordinates_Table = Table([fc1[:,0]-fw1[:,0],fc1[:,0]+fw1[:,0],fc1[:,1]-fw1[:,1],
-                               fc1[:,1]+fw1[:,1],fc1[:,2]-fw1[:,2],fc1[:,2]+fw1[:,2]],
+    coordinates_Table = Table([fc1[:,0]-fw1[:,0]/2.,fc1[:,0]+fw1[:,0]/2.,fc1[:,1]-fw1[:,1]/2.,
+                               fc1[:,1]+fw1[:,1]/2.,fc1[:,2]-fw1[:,2]/2.,fc1[:,2]+fw1[:,2]/2.],
                               names = ['xmin','xmax','ymin','ymax','zmin','zmax'])
     
     ascii.write(coordinates_Table,cfg.par.PD_output_dir+cfg.par.Auto_positions_file)
+
+    '''
 
     logical_Table = Table([refined[:]],names=['logical'])
     ascii.write(logical_Table,cfg.par.PD_output_dir+cfg.par.Auto_TF_file)
 
 
-    dust_dens_Table = Table([dust_density_grid[:]],names=['dust density'])
+    dust_dens_Table = Table([dust_smoothed[:]],names=['dust density'])
     ascii.write(dust_dens_Table,cfg.par.PD_output_dir+cfg.par.Auto_dustdens_file)
         
 
 
-    return refined,dust_density_grid,xmin,xmax,ymin,ymax,zmin,zmax,boost
-
+    #return refined,dust_smoothed,xmin,xmax,ymin,ymax,zmin,zmax,boost
+    return refined,dust_smoothed,fc1,fw1,boost
 
 
 def grid_coordinate_boost(xmin,xmax,ymin,ymax,zmin,zmax):

source_creation.py
@@ -13,10 +13,11 @@ import random
 import sys
 
 class Sed_Bins:
-    def __init__(self,mass,metals,age):
+    def __init__(self,mass,metals,age,fsps_zmet):
         self.mass = mass
         self.metals = metals
         self.age = age
+        self.fsps_zmet=fsps_zmet
 
 
 
@@ -52,6 +53,8 @@ def add_newstars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_list,disk
     nstars = len(stars_list)
     print 'adding new stars to the grid'
     
+    totallum_newstars = 0.
+
     for i in range(nstars):
         
         nu = stellar_nu[:]
@@ -68,20 +71,24 @@ def add_newstars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_list,disk
         fnu = fnu[::-1]
 
       
-
+        
         lum = np.absolute(np.trapz(fnu,x=nu))*stars_list[i].mass/const.msun 
         lum *= const.lsun #to get in cgs
 
         #add new stars
-
+        totallum_newstars += lum
         m.add_spherical_source(luminosity = lum,radius = 10.*const.rsun,spectrum = (nu,fnu),
                                position = stars_list[i].positions)
         
+
+                           
+
+
+    print '[source_creation/add_newstars:] totallum_newstars = ',totallum_newstars
         
     if cfg.par.COSMOFLAG == False: add_bulge_disk_stars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_list,diskstars_list,bulgestars_list,m)
             
     m.set_sample_sources_evenly(True)
-
     
 def add_bulge_disk_stars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_list,diskstars_list,bulgestars_list,m):
     print 'Non-Cosmological Simulation: Adding Disk and Bulge Stars:'
@@ -126,7 +133,10 @@ def add_bulge_disk_stars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_l
     disksource.position=disk_pos
     
     disksource.spectrum = (nu,fnu)
-            
+    
+
+    print '[source_creation/add_bulge_disk_stars:] totallum_disktars = ',disksource.luminosity[0]
+    
     print 'adding bulge stars to the grid: adding as a point source collection'
     
     
@@ -149,11 +159,15 @@ def add_bulge_disk_stars(df_nu,stellar_nu,stellar_fnu,disk_fnu,bulge_fnu,stars_l
     fnu = fnu[nu_inrange]
     bulgesource.spectrum = (nu,fnu)
 
+    print '[source_creation/add_bulge_disk_stars:] totallum_bulgetars = ',bulgesource.luminosity[0]
 
     
 
 def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
     
+
+  
+
     #calculate max and min ages
     minimum_age = 15 #Gyr - obviously too high of a number
     maximum_age = 0 #Gyr
@@ -169,8 +183,8 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
 
     nstars = len(stars_list)
     for i in range(nstars):
-        if stars_list[i].metals[0] < minimum_metallicity: minimum_metallicity = stars_list[i].metals[0]
-        if stars_list[i].metals[0] > maximum_metallicity: maximum_metallicity = stars_list[i].metals[0]
+        #if stars_list[i].metals[0] < minimum_metallicity: minimum_metallicity = stars_list[i].metals[0]
+        #if stars_list[i].metals[0] > maximum_metallicity: maximum_metallicity = stars_list[i].metals[0]
         
         if stars_list[i].mass < minimum_mass: minimum_mass = stars_list[i].mass
         if stars_list[i].mass > maximum_mass: maximum_mass = stars_list[i].mass
@@ -181,15 +195,14 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
       
 
 
-    #define the metallicity bins: we do this in log since there can be quite a spread in metallicities
+    #define the metallicity bins: we do this by saying that they are the number of metallicity bins in FSPS
 
-    delta_metallicity =  (np.log10(maximum_metallicity)-np.log10(minimum_metallicity))/cfg.par.N_METAL_BINS
-    metal_bins = np.arange(np.log10(minimum_metallicity),
-                           np.log10(maximum_metallicity),
-                           delta_metallicity)
-    #add on the maximum metallicity bin
-    metal_bins = np.append(metal_bins,metal_bins[-1]+delta_metallicity)
-    metal_bins = 10.**metal_bins
+    fsps_metals = np.loadtxt(cfg.par.metallicity_legend)
+    N_METAL_BINS = len(fsps_metals)
+
+    #note the bins are NOT metallicity, but rather the zmet keys in
+    #fsps (i.e. the zmet column in Table 1 of the fsps manual)
+    metal_bins = np.arange(N_METAL_BINS)+1
 
 
     #define the age bins (linearly)
@@ -222,7 +235,7 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
     #says whether or not that bin is being used downstream for
     #creating a point source collection (i.e. that it actually has at
     #least one star cluster that falls into it)
-    has_stellar_mass = np.zeros([cfg.par.N_METAL_BINS+1,cfg.par.N_STELLAR_AGE_BINS+1,cfg.par.N_MASS_BINS+1],dtype=bool)
+    has_stellar_mass = np.zeros([N_METAL_BINS,cfg.par.N_STELLAR_AGE_BINS+1,cfg.par.N_MASS_BINS+1],dtype=bool)
 
 
     stars_in_bin = {} #this will be a dictionary that holds the list
@@ -233,7 +246,7 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
     
     for i in range(nstars):
         
-        wz = find_nearest(metal_bins,stars_list[i].metals[0])
+        wz = find_nearest(metal_bins,stars_list[i].fsps_zmet)
         wa = find_nearest(age_bins,stars_list[i].age)
         wm = find_nearest(mass_bins,stars_list[i].mass)
         
@@ -261,13 +274,15 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
 
 
    
-
-    for wz in range(cfg.par.N_METAL_BINS+1):
+    #we loop through age bins +1 because the max values were tacked
+    #onto those bin lists. but for metal bins, this isn't the case, so
+    #we don't loop the extra +1
+    for wz in range(N_METAL_BINS):
         for wa in range(cfg.par.N_STELLAR_AGE_BINS+1):
             for wm in range(cfg.par.N_MASS_BINS+1):
-                sed_bins_list.append(Sed_Bins(mass_bins[wm],metal_bins[wz],age_bins[wa]))
+                sed_bins_list.append(Sed_Bins(mass_bins[wm],fsps_metals[wz],age_bins[wa],metal_bins[wz]))
                 if has_stellar_mass[wz,wa,wm] == True:
-                    sed_bins_list_has_stellar_mass.append(Sed_Bins(mass_bins[wm],metal_bins[wz],age_bins[wa]))
+                    sed_bins_list_has_stellar_mass.append(Sed_Bins(mass_bins[wm],fsps_metals[wz],age_bins[wa],metal_bins[wz]))
 
    
 
@@ -283,6 +298,9 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
 
 
 
+   
+
+
     #since the binned_stellar_fnu_has_stellar_mass is now
     #[len(sed_bins_list_has_stellar_mass),nlam)] big, we need to
     #transform it back to the a larger array.  this is an ugly loop
@@ -293,13 +311,16 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
 
     counter = 0
     counter_has_stellar_mass = 0
-    for wz in range(cfg.par.N_METAL_BINS+1):
+    for wz in range(N_METAL_BINS):
         for wa in range(cfg.par.N_STELLAR_AGE_BINS+1):
             for wm in range(cfg.par.N_MASS_BINS+1):
                 if has_stellar_mass[wz,wa,wm] == True:
                     binned_stellar_fnu[counter,:] = binned_stellar_fnu_has_stellar_mass[counter_has_stellar_mass,:]
                     counter_has_stellar_mass += 1 
                 counter+=1
+
+
+
 
 
     
@@ -329,8 +350,11 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
     t1=datetime.now()
 
 
+    totallum = 0
+    totalmass = 0
+
     counter=0
-    for wz in range(cfg.par.N_METAL_BINS+1):
+    for wz in range(N_METAL_BINS):
         for wa in range(cfg.par.N_STELLAR_AGE_BINS+1):
             for wm in range(cfg.par.N_MASS_BINS+1):
                 
@@ -342,16 +366,23 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
                     fnu = fnu[nu_inrange]
                     fnu = fnu[::-1]
                     pos = np.zeros([len(stars_in_bin[(wz,wa,wm)]),3])
-
-
+                    
+                    #quickly calculate the total stellar mass in the wz,wa,wm bin:
+                    mass = np.zeros(len(stars_in_bin[(wz,wa,wm)]))
+                    for i in range(len(stars_in_bin[(wz,wa,wm)])): mass[i] = stars_list[i].mass
+                    totalmass += np.sum(mass)
                     
                         
-                    lum = np.absolute(np.trapz(fnu,x=nu))*mass_bins[wm]/const.msun*const.lsun
+                    #lum = np.absolute(np.trapz(fnu,x=nu))*mass_bins[wm]/const.msun*const.lsun
+                    lum = (np.absolute(np.trapz(fnu,x=nu))*np.sum(mass))/const.msun*const.lsun
                     source.luminosity = np.repeat(lum,len(stars_in_bin[(wz,wa,wm)]))
                     for i in range(len(stars_in_bin[(wz,wa,wm)])): pos[i,:] = stars_list[i].positions
                     source.position=pos
-                    source.spectrum = (nu,fnu)
 
+
+                    source.spectrum = (nu,fnu)
+                                    
+                    totallum += source.luminosity[0]
 
                     if np.isnan(lum): 
                         print 'lum is a nan in point source collection addition. exiting now.'
@@ -368,8 +399,8 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
     #m.set_sample_sources_evenly(True)
 
     t2=datetime.now()
-    print 'Execution time for point source collection adding = '+str(t2-t1)
-
+    print '[source_creation/add_binned_seds:] Execution time for point source collection adding = '+str(t2-t1)
+    print '[source_creation/add_binned_seds:] Total Luminosity of point source collection is: ',totallum
 
     
 def find_nearest(array,value):