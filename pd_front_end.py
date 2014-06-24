93f3b63165f8b1db71cb8e86b5769a4f06ac31c0
desika <none@none>
2014-06-24 15:16:11 -0400

    small edits to the README

--- pd_front_end.py
+++ pd_front_end.py
@@ -260,7 +260,7 @@ image = m.add_peeled_images(sed = True,image=False)
 image.set_wavelength_range(250,0.01,5000.)
 #image.set_wavelength_range(50,0.01,5000.)
 image.set_viewing_angles(np.linspace(0,90,par.NTHETA),np.repeat(20,par.NTHETA))
-#image.set_track_origin('basic')
+image.set_track_origin('detailed')
 
 #image.set_image_size(128,128)
 #image.set_image_limits(-10.e3*const.pc,10.e3*const.pc,-10.e3*const.pc,10.e3*const.pc)
