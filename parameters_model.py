4be96121f513d7bb27a84cc91970394a3fb71a93
desika <none@none>
2014-06-25 14:38:31 -0400

    re-wrote parameters_model.py to make changing the snapnumber super easy. only really is important for typical gadget nomenclature.

--- parameters_model.py
+++ parameters_model.py
@@ -1,19 +1,19 @@
 #snapshot parameters
 
-hydro_dir = '/Users/desika/gadgetruns/sbw_mergers/mw_e_hr_DIND/'
-Gadget_snap_num = 23
-Gadget_snap_name = 'snapshot_023.hdf5'
+Gadget_snap_num = 239
+hydro_dir = '/data/desika/gadgetruns/m13_mr_Dec16_2013/snapdir_'+str(Gadget_snap_num)+'/'
+
+Gadget_snap_name = 'snapshot_'+str(Gadget_snap_num)+'.0.hdf5'
 
 #where the files should go
-PD_output_dir = '/Users/desika/Dropbox/powderday/pd_runs/sbw_tests/mw_18_6_hr_hightimeres/'
-
-Auto_TF_file = 'snap23.logical'
-Auto_dustdens_file = 'snap23.dustdens'
+PD_output_dir = '/home/desika/Dropbox/powderday/pd_runs/m13_mr_Dec16_2013/'
+Auto_TF_file = 'snap'+str(Gadget_snap_num)+'.logical'
+Auto_dustdens_file = 'snap'+str(Gadget_snap_num)+'.dustdens'
 
 
 #===============================================
 #FILE I/O
 #===============================================
-inputfile = PD_output_dir+'/example.rtin'
-outputfile = PD_output_dir+'/example.rtout'
+inputfile = PD_output_dir+'/example.'+str(Gadget_snap_num)+'.rtin'
+outputfile = PD_output_dir+'/example.'+str(Gadget_snap_num)+'.rtout'
 
