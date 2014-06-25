342e3319016c8ac46eb93d567fe3b8a1169796b4
desika <none@none>
2014-06-25 15:24:28 -0400

    changed parameters_model to deal with snaps with numbers less than 3 digits (gadget notation...)

--- parameters_model.py
+++ parameters_model.py
@@ -1,19 +1,27 @@
 #snapshot parameters
 
-Gadget_snap_num = 239
-hydro_dir = '/data/desika/gadgetruns/m13_mr_Dec16_2013/snapdir_'+str(Gadget_snap_num)+'/'
+Gadget_snap_num = 20
 
-Gadget_snap_name = 'snapshot_'+str(Gadget_snap_num)+'.0.hdf5'
+if Gadget_snap_num < 10:
+    snapnum_str = '00'+str(Gadget_snap_num)
+elif Gadget_snap_num >= 10 and Gadget_snap_num <100:
+    snapnum_str = '0'+str(Gadget_snap_num)
+else:
+    snapnum_str = str(Gadget_snap_num)
+
+hydro_dir = '/data/desika/gadgetruns/m13_mr_Dec16_2013/snapdir_'+snapnum_str+'/'
+
+Gadget_snap_name = 'snapshot_'+snapnum_str+'.0.hdf5'
 
 #where the files should go
 PD_output_dir = '/home/desika/Dropbox/powderday/pd_runs/m13_mr_Dec16_2013/'
-Auto_TF_file = 'snap'+str(Gadget_snap_num)+'.logical'
-Auto_dustdens_file = 'snap'+str(Gadget_snap_num)+'.dustdens'
+Auto_TF_file = 'snap'+snapnum_str+'.logical'
+Auto_dustdens_file = 'snap'+snapnum_str+'.dustdens'
 
 
 #===============================================
 #FILE I/O
 #===============================================
-inputfile = PD_output_dir+'/example.'+str(Gadget_snap_num)+'.rtin'
-outputfile = PD_output_dir+'/example.'+str(Gadget_snap_num)+'.rtout'
+inputfile = PD_output_dir+'/example.'+snapnum_str+'.rtin'
+outputfile = PD_output_dir+'/example.'+snapnum_str+'.rtout'
 
