976fa73f78c87dc643539c35c9699b464b31b16d
desika <none@none>
2014-09-24 13:21:03 -0400

    Cosmological stellar ages include scale factor (as opposed to gyr) and are parallelized via pool.map multithreading

--- SED_gen.py
+++ SED_gen.py
@@ -83,7 +83,17 @@ def star_list_gen(boost,xcent,ycent,zcent,dx,dy,dz):
         print '--------------\n'
     else:
         simtime = cosmo.Planck13.age(pf.current_redshift).value #what is the age of the Universe right now?
-        age = simtime-ad[("PartType4","StellarFormationTime")].value * cfg.par.unit_age #gyr
+        scalefactor = ad[("PartType4","StellarFormationTime")].value
+        formation_z = (1./scalefactor)-1.
+
+        formation_time = redshift_multithread(formation_z)
+        
+        age = simtime - formation_time
+        #make the minimum age 1 million years 
+        age[np.where(age < 1.e-3)[0]] = 1.e-3
+
+        #age = simtime-ad[("PartType4","StellarFormationTime")].value/0.7 * cfg.par.unit_age #gyr
+      
         print '\n--------------'
         print '[SED_gen/star_list_gen: ] Cosmological Galaxy Simulation Assumed: Current age of Universe is (Assuming Planck13 Cosmology) is (Gyr): ',simtime
         print '--------------\n'
@@ -112,7 +122,7 @@ def star_list_gen(boost,xcent,ycent,zcent,dx,dy,dz):
     zmet = fsps_metallicity_interpolate(metals)
     #mwd(zmet,mass,'zmet_distribution.png')
 
-    print '[SED_gen/star_list_gen: ] fsps zmet codes:',zmet
+    #print '[SED_gen/star_list_gen: ] fsps zmet codes:',zmet
 
     #create the stars_list full of Stars objects
     stars_list = []
@@ -293,6 +303,7 @@ def allstars_sed_gen(stars_list,diskstars_list,bulgestars_list):
     #because this can result in too many chunks sometimes given the number of processors:
     chunk_start_indices = chunk_start_indices[0:nchunks]
     '''
+    print 'Entering Pool.map multiprocessing for Stellar SED generation'
     list_of_chunks = []
     for n in range(nchunks):
         stars_list_chunk = stars_list[chunk_start_indices[n]:chunk_start_indices[n]+delta_chunk_indices]
@@ -304,7 +315,7 @@ def allstars_sed_gen(stars_list,diskstars_list,bulgestars_list):
     
     
 
-        print 'Entering Pool.map multiprocessing'
+        
     t1=datetime.now()
     chunk_sol = p.map(newstars_gen, [arg for arg in list_of_chunks])
     
@@ -373,8 +384,46 @@ def allstars_sed_gen(stars_list,diskstars_list,bulgestars_list):
     return stellar_nu,stellar_fnu,disk_fnu,bulge_fnu
 
 
+def redshift_multithread(formation_z):
 
+        formation_z_list = formation_z
+        #initialize the process pool and build the chunks
+        p = Pool(processes = cfg.par.n_processes)
+        nchunks = cfg.par.n_processes
+        chunk_start_indices = []
+        chunk_start_indices.append(0) #the start index is obviously 0
+        delta_chunk_indices = int(len(formation_z_list) / nchunks)
+        print 'delta_chunk_indices = ',delta_chunk_indices
+        for n in range(1,nchunks):
+            chunk_start_indices.append(chunk_start_indices[n-1]+delta_chunk_indices)
+        list_of_chunks = []
+        for n in range(nchunks):
+            formation_z_chunk = formation_z_list[chunk_start_indices[n]:chunk_start_indices[n]+delta_chunk_indices]
+            if n == nchunks-1: 
+                formation_z_chunk = formation_z_list[chunk_start_indices[n]::]
+            list_of_chunks.append(formation_z_chunk)
+        print 'Entering Pool.map multiprocessing for Stellar Age calculations'
+        t1=datetime.now()
+        chunk_sol = p.map(redshift_gen, [arg for arg in list_of_chunks])
+        
 
+        formation_time = []
+        for i in range(len(chunk_sol)):
+            sub_chunk_sol = chunk_sol[i].value
+            for j in range(len(sub_chunk_sol)):
+                formation_time.append(sub_chunk_sol[j])
+
+        t2=datetime.now()
+        print 'Execution time for Stellar Age calculations in Pool.map multiprocessing = '+str(t2-t1)
+        
+      
+        
+        return np.array(formation_time)
+        
+
+def redshift_gen(formation_z):
+    age = cosmo.Planck13.age(formation_z)
+    return age
 
 def newstars_gen(stars_list):
     #the newstars (particle type 4; so, for cosmological runs, this is all
