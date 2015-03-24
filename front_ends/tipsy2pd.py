5b5c89c7567043018fcc1aa7c731ddef8ae45915
desika <none@none>
2015-03-24 15:09:46 -0400

    tipsy front end built for christensen idealized galaxy test

--- front_ends/tipsy2pd.py
+++ front_ends/tipsy2pd.py
@@ -2,6 +2,7 @@ import numpy as np
 import astropy.units as u
 import yt
 from yt import derived_field
+import ipdb
 
 
 
@@ -14,14 +15,68 @@ from yt import derived_field
 #6. Parttype0_Coordinates
 #7. Parttype0_Smoothed_Density
 
+def tipsy_field_add(fname,unit_base,bbox,ds=None,starages=False):
 
-def _starmetals(field,data):
-    #return (data["PartType0","Density"]*data["PartType0","Metallicity"])
-    return data[('Stars', 'Metals')]
+    def _starmetals(field,data):
+        return data[('Stars', 'Metals')]
+    
+    def _starcoordinates(field,data):
+        return data[('Stars', 'Coordinates')]
 
-yt.add_field(('starmetals'),function=_starmetals,units="code_metallicity",particle_type=True)
+    def _starformationtime(field,data):
+        return data[('Stars', 'FormationTime')]
 
-ds = yt.load('../tipsy/TipsyGalaxy/galaxy.00300')
-ds.index
-ad = ds.all_data()
-print ad[("starmetals")]
+    def _stellarages(field,data):
+        ad = data.ds.all_data()
+        simtime = data.ds.current_time.in_units('Gyr')
+        simtime = simtime.value
+        age = simtime - ad["starformationtime"].value
+        age[np.where(age < 1.e-3)[0]] = 1.e-3
+        return age
+
+    def _starmasses(field,data):
+        return data[('Stars', 'Mass')]
+    
+    def _gasdensity(fied,data):
+        return data[('Gas', 'Density')]
+        
+    def _gasmetals(field,data):
+        return data[('Gas', 'Metals')]
+        
+    def _gascoordinates(field,data):
+        return data[('Gas','Coordinates')]
+
+    def _gassmootheddensity(field,data):
+        return data[('deposit', 'Gas_density')]
+    
+    def _gassmoothedmetals(field,data):
+        return data[('deposit', 'Gas_smoothed_metallicity')]
+        
+    def _gassmoothedmasses(field,data):
+        return data[('deposit', 'Gas_mass')]
+
+    def _metaldens(field,data):
+        return (data["Gas","Density"]*data["Gas","Metals"])
+
+
+    ds = yt.load('../tipsy/christensen/Disk_Collapse_1e6.00100.scalez1')
+    ds.index
+    
+    ds.add_field(('starmetals'),function=_starmetals,units="code_metallicity",particle_type=True)
+    ds.add_field(('starcoordinates'),function=_starcoordinates,units='cm',particle_type=True)
+    ds.add_field(('starformationtime'),function=_starformationtime,units='Gyr',particle_type=True)
+    ds.add_field(('stellarages'),function=_stellarages,units='Gyr',particle_type=True)
+    ds.add_field(('starmasses'),function=_starmasses,units='g',particle_type=True)
+    ds.add_field(('gasdensity'),function=_gasdensity,units='g/cm**3',particle_type=True)
+    ds.add_field(('gasmetals'),function=_gasmetals,units="code_metallicity",particle_type=True)
+    ds.add_field(('gascoordinates'),function=_gascoordinates,units='cm',particle_type=True)
+    ds.add_field(('gassmootheddensity'),function=_gassmootheddensity,units='g/cm**3',particle_type=True)
+    ds.add_field(('gassmoothedmetals'),function=_gassmoothedmetals,units='code_metallicity',particle_type=True)
+    ds.add_field(('metaldens'),function=_metaldens,units="g/cm**3", particle_type=True)
+    
+
+
+    ad = ds.all_data()
+    
+    return ds
+
