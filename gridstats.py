e6531c4cf12c54fbdfcf19d0dc2d94a24f273359
desika <none@none>
2015-03-25 10:30:10 -0400

    cleaned up units in gridstats

--- gridstats.py
+++ gridstats.py
@@ -5,7 +5,7 @@ import constants as const
 
 def gridstats(ir1,fc1,fw1):
 
-    volume = (fw1 * const.pc* 1.e3)**3.
+    volume = (fw1**3)
     
     xmin = fc1[:,0]-fw1[:,0]/2.
     xmax = fc1[:,0]+fw1[:,0]/2.
@@ -19,16 +19,16 @@ def gridstats(ir1,fc1,fw1):
     print 'Grid Statistics'
     print '----------------------------'
     
-    print 'Smallest Cell Edge (kpc): ',np.min(fw1)
-    print 'Smallest Cell Volume (cm^3): ',np.min(volume)
+    print 'Smallest Cell Edge: ',np.min(fw1.in_units('kpc'))
+    print 'Smallest Cell Volume: ',np.min(volume.in_units('kpc**3'))
 
-    print 'Biggest Cell Edge (kpc): ',np.max(fw1)
-    print 'Biggest Cell Volume (cm^3): ',np.max(volume)
+    print 'Biggest Cell Edge: ',np.max(fw1.in_units('kpc'))
+    print 'Biggest Cell Volume: ',np.max(volume.in_units('kpc**3'))
    
-    print 'Left Edge (kpc): ',np.min(xmin)
-    print 'Right Edge (kpc): ',np.max(xmax)
-    print 'Bottom Edge (kpc): ',np.min(ymin)
-    print 'Top Edge (kpc): ',np.max(ymax)
-    print 'Nearest Edge (kpc): ',np.min(zmin)
-    print 'Farthest Edge (kpc): ',np.max(ymax)
+    print 'Left Edge: ',np.min(xmin.in_units('kpc'))
+    print 'Right Edge: ',np.max(xmax.in_units('kpc'))
+    print 'Bottom Edge: ',np.min(ymin.in_units('kpc'))
+    print 'Top Edge: ',np.max(ymax.in_units('kpc'))
+    print 'Nearest Edge: ',np.min(zmin.in_units('kpc'))
+    print 'Farthest Edge: ',np.max(ymax.in_units('kpc'))
     
