4a713843f02fb909d5a67d3d3d5e231f4d4762e4
desika <none@none>
2016-02-05 16:32:17 -0500

    fixed a bug introduced in last commit

--- source_creation.py
+++ source_creation.py
@@ -206,7 +206,7 @@ def add_binned_seds(df_nu,stars_list,diskstars_list,bulgestars_list,m):
 
 
     #define the age bins (linearly)
-    #delta_age = (maximum_age-minimum_age)/cfg.par.N_STELLAR_AGE_BINS
+    delta_age = (maximum_age-minimum_age)/cfg.par.N_STELLAR_AGE_BINS
     #age_bins = np.arange(minimum_age,maximum_age,delta_age)
     age_bins = np.linspace(minimum_age,maximum_age,cfg.par.N_STELLAR_AGE_BINS)
     #tack on the maximum age bin
