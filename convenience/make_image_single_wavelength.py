4e7dde39de7418f8fa25e28e676bc927c29730ea
mccbc <cmcclellan1010@gmail.com>
2019-06-06 15:09:54 -0400

    removed line break for long path in imaging script, changed get_image units keyword to ergs/s

--- convenience/make_image_single_wavelength.py
+++ convenience/make_image_single_wavelength.py
@@ -7,15 +7,13 @@ import astropy.units as u
 # modifiable header
 # ------------------------
 
-m = ModelOutput(
-    '/Users/desika/Dropbox/powderday/verification/gadget/example.200.rtout.image')
+m = ModelOutput('/Users/desika/Dropbox/powderday/verification/gadget/example.200.rtout.image')
 wav = 200  # micron
 
 # ------------------------
 
-# Extract the image for the first inclination, and scale to 300pc. We
-# have to specify group=1 as there is no image in group 0.
-image = m.get_image(units='mJy')
+# Get the image from the ModelOutput object
+image = m.get_image(units='ergs/s')
 
 # Open figure and create axes
 fig = plt.figure()
@@ -29,7 +27,7 @@ w = image.x_max * u.cm
 w = w.to(u.kpc)
 
 # plot the beast
-cax = ax.imshow(np.log(image.val[0, :, :, iwav]), cmap=plt.cm.magma,
+cax = ax.imshow(np.log(image.val[0, :, :, iwav]), cmap=plt.cm.viridis,
                 origin='lower', extent=[-w.value, w.value, -w.value, w.value])
 
 
