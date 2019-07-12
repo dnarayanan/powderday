1704ba9af90635f5f7f065b9b32b5b950ef7a83d
cmcclellan1010 <none@none>
2019-07-12 12:51:11 -0400

    changed sum to np.sum in sph tributary

--- sph_tributary.py
+++ sph_tributary.py
@@ -119,7 +119,7 @@ def sph_m_gen(fname,field_add):
         frac = cfg.par.PAH_frac
 
         # Normalize to 1
-        total = sum(frac.values())
+        total = np.sum(frac.values())
         frac = {k: v / total for k, v in frac.items()}
 
         for size in frac.keys():
