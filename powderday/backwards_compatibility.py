bc2371625f1a011e6bd23f36a95be5bd0f55851a
prerakgarg07 <prerakgarg07@gmail.com>
2019-10-18 09:01:46 -0400

    Fixed incorrect variable names in backwards_compatibility.py

--- powderday/backwards_compatibility.py
+++ powderday/backwards_compatibility.py
@@ -132,49 +132,49 @@ def variable_set():
         cfg.par.BH_var = False
 
     try:
-        FORCE_STELLAR_AGES
+        cfg.par.FORCE_STELLAR_AGES
     except:
-        FORCE_STELLAR_AGES = False
+        cfg.par.FORCE_STELLAR_AGES = False
 
     try:
-        FORCE_STELLAR_AGES_VALUE
+        cfg.par.FORCE_STELLAR_AGES_VALUE
     except:
-        FORCE_STELLAR_AGES_VALUE = 1
+        cfg.par.FORCE_STELLAR_AGES_VALUE = 1
 
     try:
-        FORCE_STELLAR_METALLICITIES
+        cfg.par.FORCE_STELLAR_METALLICITIES
     except:
-        FORCE_STELLAR_METALLICITIES = False
+        cfg.par.FORCE_STELLAR_METALLICITIES = False
 
     try:
-        FORCE_STELLAR_METALLICITIES_VALUE
+        cfg.par.FORCE_STELLAR_METALLICITIES_VALUE
     except:
-        FORCE_STELLAR_METALLICITIES_VALUE = 0.013
+        cfg.par.FORCE_STELLAR_METALLICITIES_VALUE = 0.013
 
     try:
-        HII_T
+        cfg.par.HII_T
     except:
-        HII_T = 1.e4
+        cfg.par.HII_T = 1.e4
 
     try: 
-        HII_nh
+        cfg.par.HII_nh
     except:
-        HII_nh = 1.e2
+        cfg.par.HII_nh = 1.e2
 
     try:
-        HII_max_age
+        cfg.par.HII_max_age
     except: 
-        HII_max_age = 2.e-3
+        cfg.par.HII_max_age = 2.e-3
         
     try:
-        neb_file_output
+        cfg.par.neb_file_output
     except:
-        neb_file_output = True
+        cfg.par.neb_file_output = True
 
     try:
-        stellar_cluster_mass
+        cfg.par.stellar_cluster_mass
     except:
-        stellar_cluster_mass = 1.e4
+        cfg.par.stellar_cluster_mass = 1.e4
 
     try:
         cfg.par.filterdir
@@ -195,4 +195,4 @@ def variable_set():
     except:
         cfg.par.PAH_frac = {'usg': 0.0586, 'vsg': 0.1351, 'big': 0.8063}
 
-    return cfg.par.FORCE_RANDOM_SEED,cfg.par.BH_SED,cfg.par.IMAGING,cfg.par.SED,cfg.par.IMAGING_TRANSMISSION_FILTER,cfg.par.SED_MONOCHROMATIC,cfg.par.SKIP_RT,cfg.par.FIX_SED_MONOCHROMATIC_WAVELENGTHS,cfg.par.n_MPI_processes,cfg.par.SOURCES_RANDOM_POSITIONS,cfg.par.FORCE_gas_logu,cfg.par.gas_logu,cfg.par.gas_logz,cfg.par.FORCE_gas_logz,cfg.par.SUBLIMATION,cfg.par.SUBLIMATION_TEMPERATURE,cfg.model.TCMB,cfg.model.THETA,cfg.model.PHI,cfg.par.MANUAL_ORIENTATION,cfg.par.solar,cfg.par.dust_grid_type,cfg.par.BH_model,cfg.par.BH_modelfile,cfg.par.BH_var,cfg.par.FORCE_STELLAR_AGES,cfg.par.FORCE_STELLAR_AGES_VALUE,cfg.par.FORCE_STELLAR_METALLICITIES,cfg.par.FORCE_STELLAR_METALLICITIES_VALUE,HII_T,HII_nh,HII_max_age,neb_file_output,stellar_cluster_mass, cfg.par.filterdir, cfg.par.filterfiles, cfg.par.PAH_frac
+    return cfg.par.FORCE_RANDOM_SEED,cfg.par.BH_SED,cfg.par.IMAGING,cfg.par.SED,cfg.par.IMAGING_TRANSMISSION_FILTER,cfg.par.SED_MONOCHROMATIC,cfg.par.SKIP_RT,cfg.par.FIX_SED_MONOCHROMATIC_WAVELENGTHS,cfg.par.n_MPI_processes,cfg.par.SOURCES_RANDOM_POSITIONS,cfg.par.FORCE_gas_logu,cfg.par.gas_logu,cfg.par.gas_logz,cfg.par.FORCE_gas_logz,cfg.par.SUBLIMATION,cfg.par.SUBLIMATION_TEMPERATURE,cfg.model.TCMB,cfg.model.THETA,cfg.model.PHI,cfg.par.MANUAL_ORIENTATION,cfg.par.solar,cfg.par.dust_grid_type,cfg.par.BH_model,cfg.par.BH_modelfile,cfg.par.BH_var,cfg.par.FORCE_STELLAR_AGES,cfg.par.FORCE_STELLAR_AGES_VALUE,cfg.par.FORCE_STELLAR_METALLICITIES,cfg.par.FORCE_STELLAR_METALLICITIES_VALUE,cfg.par.HII_T,cfg.par.HII_nh,cfg.par.HII_max_age,cfg.par.neb_file_output,cfg.par.stellar_cluster_mass, cfg.par.filterdir, cfg.par.filterfiles, cfg.par.PAH_frac
