4f718bbab0c039e9fe2a2154cc98d31f91931744
dnarayanan <desika.narayanan@gmail.com>
2020-01-09 13:42:23 -0500

    small updates to tipsy controllers to fix bugs that prevent running

--- tests/SKIRT/changa_disk/parameters_model_changa.py
+++ tests/SKIRT/changa_disk/parameters_model_changa.py
@@ -6,7 +6,7 @@ hydro_dir = '/ufrc/narayanan/desika.narayanan/yt_datasets/TipsyGalaxy/'
 snapshot_name = 'galaxy.00300'
 
 #where the files should go
-PD_output_dir = '/ufrc/narayanan/desika.narayanan/pd/tests/SKIRT/changa_disk/'
+PD_output_dir = '/ufrc/narayanan/desika.narayanan/pd_git/tests/SKIRT/changa_disk/'
 Auto_TF_file = 'changa_test.logical'
 Auto_dustdens_file = 'changa_test.dustdens'
 
