e0e6a364f00e1ce69cd2e62e7b19682b5974964a
dnarayanan <desika.narayanan@gmail.com>
2020-01-14 20:39:49 -0500

    driver format implemented into caesar_cosmology_npzgen

--- convenience/computers/gizmo_caesar/caesar_cosmology_npzgen.py
+++ convenience/computers/gizmo_caesar/caesar_cosmology_npzgen.py
@@ -10,11 +10,12 @@ import caesar
 import ipdb
 from glob2 import glob
 
-directory = '/ufrc/narayanan/desika.narayanan/gizmo_runs/simba/m25n512/output/Groups//'
+#directory = '/ufrc/narayanan/desika.narayanan/gizmo_runs/simba/m25n512/output/Groups//'
+directory = '/orange/narayanan/desika.narayanan/gizmo_runs/simba/m25n512_filtered/output/Groups/'
 NGALAXIES_MAX = 10000
 TESTING = False
 outfile = '/ufrc/narayanan/desika.narayanan/pd_runs/simba/m25n512/simba_m25n512.galaxies_pos_for_pd.npz'
-
+DRIVER_FORMAT = False
 
 
 MEMBERS = np.sort(glob('%s/caesar*.hdf5' % (directory)))
@@ -31,8 +32,10 @@ if TESTING:MEMBERS=[MEMBERS[-1]]
 for file in MEMBERS:
 
     #this gets snaps in 3 digit format
-    snapnum = file[file.find('caesar_')+8:file.find('_z')]
-
+    if DRIVER_FORMAT: #if the caesar files were written with driver
+        snapnum = file[file.find('caesar_')+8:file.find('_z')] 
+    else: #else they were written with the CLI
+        snapnum = file[file.find('caesar_')+16:file.find('.hdf5')]
     #this try/except is in case the caesar was run on snaps so early there are no galaxies
     try:
         obj = caesar.load(file)
