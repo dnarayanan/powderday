d9a890972ef48bc7ed684199db49c57348f60a76
dnarayanan <desika.narayanan@gmail.com>
2020-01-17 16:22:25 -0500

    enzo still doesnt work - just checking in

--- powderday/enzo_tributary.py
+++ powderday/enzo_tributary.py
@@ -11,78 +11,48 @@ import yt
 
 def enzo_m_gen(fname,field_add):
     
-
-    '''
+    
     #add the fields in pd format
     ds = field_add(fname)
     
     #def. dust density
     def _dust_density(field, data):
         return data[('gas', 'metal_density')].in_units("g/cm**3")*cfg.par.dusttometals_ratio
-
     ds.add_field(('gas', 'dust_density'), function=_dust_density, units = 'g/cm**3')
 
 
     ad = ds.all_data()
    
- 
+    #now set up a region DEBUG DEBUG DEBUG THIS IS TOTALLY HACKED AND NOT THE RIGHT REGION
+    #box_len = ds.quan(0.1,'code_length')
+    #center = ds.domain_center
+    #min_region = [center[0]-box_len,center[1]-box_len,center[2]-box_len]
+    #max_region = [center[0]+box_len,center[1]+box_len,center[2]+box_len]
+    
+    #reg = ds.region(center,min_region,max_region)
 
-    #cutout
+
+    #set up the region again
     center = ds.arr([cfg.model.x_cent,cfg.model.y_cent,cfg.model.z_cent],'code_length')
-    
     box_len = ds.quan(cfg.par.zoom_box_len,'kpc').in_units('code_length')
-   
     min_region = [center[0]-box_len,center[1]-box_len,center[2]-box_len]
     max_region = [center[0]+box_len,center[1]+box_len,center[2]+box_len]
     reg = ds.region(center,min_region,max_region)
-    
-    reg.save_as_dataset('temp_enzo.h5')
+
+
+    reg.save_as_dataset('temp_enzo.h5',fields=[('all','creation_time'),('gas','metal_density'),('gas','density'),('newstars','metallicity_fraction'),('newstars','particle_mass'),('all', 'particle_index'),('index', 'grid_level')])
     ds1 = yt.load('temp_enzo.h5')
     ad1 = ds1.all_data()
     
     print(ad1[('all', 'creation_time')])
-    '''
-
-    ds = yt.load(fname)
-    ds = field_add(fname)
-
-    #DEBUG DEBUG DEBUG DEBUG ALL THIS NEEDS TO BE MERGED IN WITH WHATS ABOVE AS ITS SUPER HACKY AND PRETENDS E.G. LIKE WE KNOW WHAT THE BOX LENGTH IS ETC. ACK!!!!
-
-    #def. dust density
-    def _dust_density(field, data):
-        return data[('gas', 'metal_density')].in_units("g/cm**3")*cfg.par.dusttometals_ratio
-
-    ds.add_field(('gas', 'dust_density'), function=_dust_density, units = 'g/cm**3')
-
-
-    ad = ds.all_data()
-    print(ad[('all', 'creation_time')])
-    
-    #set up a region
-    box_len = ds.quan(0.1,'code_length')
-    center = ds.domain_center
-    min_region = [center[0]-box_len,center[1]-box_len,center[2]-box_len]
-    max_region = [center[0]+box_len,center[1]+box_len,center[2]+box_len]
-    
-    reg = ds.region(center,min_region,max_region)
-    
-    import pdb
-    pdb.set_trace()
-    
-    #now save the region to disk as a ds force the addition of some
-    #fields that don't otherwise get saved in the save_as_dataset
-    #method, because they are derived fields
-    reg.save_as_dataset('temp_enzo.h5',fields=[('all','creation_time'),('gas','metal_density')])
-    
-    #load the region back up
-    ds1 = yt.load('temp_enzo.h5')
-    ad1 = ds1.all_data()
-    print(ad1[('all', 'creation_time')])
 
     import pdb
     pdb.set_trace()
+    ds1.index.grid_levels = reg.index.grid_levels
+    ds1.index.grid_left_edge = reg.index.grid_left_edge
+    ds1.index.grid_right_edge = reg.index.grid_right_edge
+    #ds1 = field_add('temp_enzo.h5',ds=ds1,starages=True)
 
-    ds1 = field_add('temp_enzo.h5',ds=ds1,starages=True)
     amr = AMRGrid.from_yt(ds1, quantity_mapping={'density':('gas','dust_density')})
     
 
