2de623bb77d32aefe00ee08181620095a82546cd
dnarayanan <desika.narayanan@gmail.com>
2020-04-30 11:38:25 -0400

    adding arepo smuggle low res test

---
+++ tests/SKIRT/arepo_smuggle_low_res/parameters_model_arepo.py
@@ -0,0 +1,30 @@
+#snapshot parameters
+
+snapnum_str = '143'
+hydro_dir = '/ufrc/narayanan/desika.narayanan/powderday_files/smuggle/low_res/'
+
+snapshot_name = 'snapshot_143.hdf5'
+
+#where the files should go
+PD_output_dir = '/home/desika.narayanan/pd_git/tests/SKIRT/arepo_smuggle_low_res/'
+Auto_TF_file = 'halo_59.logical'
+Auto_dustdens_file = 'halo_59.dustdens'
+
+
+#===============================================
+#FILE I/O
+#===============================================
+inputfile = PD_output_dir+'/pd_skirt_comparison.'+snapnum_str+'.rtin'
+outputfile = PD_output_dir+'/pd_skirt_comparison.'+snapnum_str+'.rtout'
+
+#===============================================
+#GRID POSITIONS
+#===============================================
+x_cent=300
+y_cent= 300
+z_cent= 300
+
+#===============================================
+#CMB
+#===============================================
+TCMB = 2.73
