e3668c73a1b5137d8d53c06e77d437f1a1472ef2
Prerak Garg <prerakgarg07@gmail.com>
2020-05-17 22:16:01 -0400

    Bug fix: direct_add_stars crashed if no stars to add, added a check to fix this. Also a removed redundant flag (direct_add_stars)

--- tests/SKIRT/enzo_disk/parameters_master_enzo.py
+++ tests/SKIRT/enzo_disk/parameters_master_enzo.py
@@ -52,10 +52,11 @@ SUBLIMATION_TEMPERATURE = 1600. #K -- meaningliess if SUBLIMATION == False
 #===============================================
 #STELLAR SEDS INFO
 #===============================================
-FORCE_BINNED = True               # force all star particles to be binned for calculating SED
-direct_add_stars = False          # If True, all star particles below max_age_direct (next parameter) are added
+FORCE_BINNED = True               # If True, force all star particles to be binned for calculating SED. 
+                                  # If False, all star particles below max_age_direct (next parameter) are added 
                                   # directly without binning for calculating SED
-max_age_direct  = 1.e-2           # Age (in Gyr) below which stars will be not be binne
+max_age_direct  = 1.e-2           # Age (in Gyr) below which stars will be directly added without binning (works only if FORCE_BINNED is False)
+
 imf_type = 1 #FSPS imf types; 0 = salpeter, 1 = chabrier; 2 = kroupa; 3 and 4 (vandokkum/dave) not currently supported
 pagb = 0 #weight given to post agb stars# 1 is the default
 add_agb_dust_model=False #add circumstellar AGB dust model (100%); Villaume, Conroy & Jonson 2015
