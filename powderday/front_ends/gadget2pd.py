dnarayanan/powderday

commit 224e78a72d75f597f32b52dae00481bd583b0ffc

updating conditional for yt4.x to be more general and robust against yt version changes

powderday/front_ends/gadget2pd.py
@@ -91,15 +91,15 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
 
     def _gassfr(field, data):
         return data[('PartType0', 'StarFormationRate')]
-
+        
     def _gassmootheddensity(field, data):
-        if  yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             return data.ds.parameters['octree'][('PartType0', 'density')]
         else:
             return data[("deposit","PartType0_smoothed_density")]
-
+            
     def _gassmoothedmetals(field, data):
-        if  yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             return data.ds.parameters['octree'][('PartType0', 'metallicity')]
         else:
             el_str = field.name[1]
@@ -110,7 +110,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
             return data[("deposit","PartType0_smoothed_"+el_name+"metallicity")]
 
     def _gassmoothedmasses(field, data):
-        if  yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             return data.ds.parameters['octree'][('PartType0', 'Masses')]
         else:
             return data[('deposit', 'PartType0_mass')]
@@ -128,7 +128,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
         return (data["PartType0", "Masses"]*(data["PartType0", "Metallicity"].value))
 
     def _metalsmoothedmasses(field, data):
-        if  yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             return (data.ds.parameters['octree'][('PartType0', 'Masses')]* data.ds.parameters['octree'][('PartType0','metallicity')])
         else:
             return (data[('deposit', 'PartType0_smoothed_metalmass')].value)
@@ -164,7 +164,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
         return dust_to_gas_ratio * data["gasmasses"]
 
     def _dustsmoothedmasses(field, data):
-        if yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             if cfg.par.otf_extinction == True:
                 dsm = ds.arr(data.ds.parameters['octree'][('PartType3','Masses')],'code_mass')
             else:
@@ -182,7 +182,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
 
 
     def _li_ml_dustsmoothedmasses(field,data):
-        if yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             return (data.ds.parameters['octree'][('PartType0', 'li_ml_dustmass')])
         else:
             return (data.ds.arr(data[("deposit", "PartType0_sum_li_ml_dustmass")].value, 'code_mass'))
@@ -293,7 +293,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
     
     # load the ds (but only if this is our first passthrough and we pass in fname)
     if fname != None:
-        if  yt.__version__ == '4.0.dev0':
+        if float(yt.__version__[0:3]) >= 4:
             ds = yt.load(fname)
             
             #ds.sph_smoothing_style = "gather"
@@ -313,7 +313,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
 
 
     #if we're in the 4.x branch of yt, load up the octree for smoothing
-    if  yt.__version__ == '4.0.dev0':
+    if float(yt.__version__[0:3]) >= 4:
         left = np.array([pos[0] for pos in bounding_box])
         right = np.array([pos[1] for pos in bounding_box])
         #octree = ds.octree(left, right, over_refine_factor=cfg.par.oref, n_ref=cfg.par.n_ref, force_build=True)
@@ -519,7 +519,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
                 ds.add_field(('PartType3','dummy_size_bin'+str(isize)),function=_size_with_units,sampling_type='particle',units='dimensionless',particle_type=True,force_override=True)
 
                 #deposit onto the octree.
-                if yt.__version__ != '4.0.dev0':
+                if float(yt.__version__[0:3]) >= 4:
                     ds.add_deposited_particle_field(('PartType3','dummy_size_bin'+str(isize)),"sum")
                     
                     print(np.sum(ad["PartType3","dummy_size_bin"+str(isize)]))
@@ -537,12 +537,12 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
                 #in zoom.py
 
                 if isize == 0:
-                    if  yt.__version__ != '4.0.dev0':
+                    if float(yt.__version__[0:3]) >= 4:
                         octree_of_sizes = np.zeros((ad[('deposit','PartType3_sum_dummy_size_bin'+str(isize))].shape[0],nsizes))
                     else:
                         octree_of_sizes = np.zeros((len(octree[('PartType3','dummy_size_bin'+str(isize))]),nsizes))
 
-                if  yt.__version__ != '4.0.dev0':
+                if float(yt.__version__[0:3]) >= 4:
                     octree_of_sizes[:,isize] = ad[('deposit','PartType3_sum_dummy_size_bin'+str(isize))]
                 else:
                     octree_of_sizes[:,isize] = octree[('PartType3','dummy_size_bin'+str(isize))]

powderday/front_ends/arepo2pd.py
@@ -206,7 +206,7 @@ def arepo_field_add(fname, bounding_box=None, ds=None):
     # load the ds (but only if this is our first passthrough and we pass in fname)
     if fname != None:
         try:
-            yt.__version__ == '4.0.dev0'
+            float(yt.__version__[0:3]) >= 4
             ds = yt.load(fname)
             ds.index
             ad = ds.all_data()

powderday/grid_construction.py
@@ -64,7 +64,7 @@ def yt_octree_generate(fname, field_add):
     #pu = ParticleUnion("all", list(ds.particle_types_raw))
 
     
-    if  yt.__version__ == '4.0.dev0':
+    if float(yt.__version__[0:3]) >= 4:
         refined = reg.parameters['octree'][('index','refined')].astype('bool')
 
         #get central positions of cells as edges + width/2.

powderday/sph_tributary.py
@@ -34,7 +34,7 @@ def sph_m_gen(fname,field_add):
     
     refined,dustdens,fc1,fw1,reg,ds = yt_octree_generate(fname,field_add)
     
-    if yt.__version__ == '4.0.dev0':
+    if float(yt.__version__[0:3]) >= 4:
         xmin = (fc1[:,0]-fw1[:,0]/2.).to('cm') #in proper cm 
         xmax = (fc1[:,0]+fw1[:,0]/2.).to('cm')
         ymin = (fc1[:,1]-fw1[:,1]/2.).to('cm')

powderday/zoom.py
@@ -48,7 +48,7 @@ def octree_zoom_bbox_filter(fname,ds,bbox0,field_add):
     #yt 3.x
     box_len = ds.quan(box_len,'kpc')
     #yt 4.x
-    if yt.__version__ == '4.0.dev0':
+    if float(yt.__version__[0:3]) >= 4:
         box_len = float(box_len.to('code_length').value)
         bbox_lim = box_len
     else:
@@ -74,7 +74,7 @@ def octree_zoom_bbox_filter(fname,ds,bbox0,field_add):
     #dataset.  We pass around the octree itself in a newly created
     #dictionary called reg.parameters
 
-    if yt.__version__ == '4.0.dev0':
+    if float(yt.__version__[0:3]) >= 4:
         
         #re load the field names, but now with the bounding box
         #set. this will allow us to map the field names to those

commit c30f1a998a427f8d46165a6fc7d0a6236d5160c5

fixing a bug introduced in commit 224e78a72d75f597f32b52dae00481bd583b0ffc

powderday/front_ends/gadget2pd.py
@@ -519,7 +519,7 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
                 ds.add_field(('PartType3','dummy_size_bin'+str(isize)),function=_size_with_units,sampling_type='particle',units='dimensionless',particle_type=True,force_override=True)
 
                 #deposit onto the octree.
-                if float(yt.__version__[0:3]) >= 4:
+                if float(yt.__version__[0:3]) < 4:
                     ds.add_deposited_particle_field(('PartType3','dummy_size_bin'+str(isize)),"sum")
                     
                     print(np.sum(ad["PartType3","dummy_size_bin"+str(isize)]))
@@ -537,12 +537,12 @@ def gadget_field_add(fname, bounding_box=None, ds=None,add_smoothed_quantities=T
                 #in zoom.py
 
                 if isize == 0:
-                    if float(yt.__version__[0:3]) >= 4:
+                    if float(yt.__version__[0:3]) < 4:
                         octree_of_sizes = np.zeros((ad[('deposit','PartType3_sum_dummy_size_bin'+str(isize))].shape[0],nsizes))
                     else:
                         octree_of_sizes = np.zeros((len(octree[('PartType3','dummy_size_bin'+str(isize))]),nsizes))
 
-                if float(yt.__version__[0:3]) >= 4:
+                if float(yt.__version__[0:3]) < 4:
                     octree_of_sizes[:,isize] = ad[('deposit','PartType3_sum_dummy_size_bin'+str(isize))]
                 else:
                     octree_of_sizes[:,isize] = octree[('PartType3','dummy_size_bin'+str(isize))]