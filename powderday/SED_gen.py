Fixed units and mass is now rescaled

powderday/SED_gen.py
@@ -459,15 +459,19 @@ def newstars_gen(stars_list):
                     age_clusters = []
                     for k in range(len(cluster_mass_cmdf)):
                         num, t = age_dist(num_clusters_cmdf[k], stars_list[i].age)
+                        rescale = np.sum(num_clusters_cmdf[k])/np.sum(num)
+                        
                         for l in range(len(num)):
+                            if num[l] == 0:
+                                continue
                             num_clusters.append(num[l])
-                            cluster_mass.append(cluster_mass_cmdf[k])
+                            cluster_mass.append(cluster_mass_cmdf[k]*rescale)
                             age_clusters.append(t[l])
 
                 cluster_mass = np.array(cluster_mass)
                 num_clusters = np.array(num_clusters)
                 age_clusters = np.array(age_clusters)
-
+            
             f = np.zeros(nlam)
             cloudy_nlam = len(np.genfromtxt(cfg.par.pd_source_dir + "/powderday/nebular_emission/data/refLines.dat", delimiter=','))
             line_em = np.zeros([cloudy_nlam])
@@ -622,7 +626,7 @@ def newstars_gen(stars_list):
                 #the stellar population returns the calculation in units of Lsun/1 Msun: https://github.com/dfm/python-fsps/issues/117#issuecomment-546513619
                 line_em = line_em * (stars_list[i].mass * u.g).to(u.Msun).value * 3.839e33  # Units: ergs/s
                 line_em = np.append(line_em, age)
-                dump_emlines(wave_line, line_em, id_val)
+                dump_emlines(wave_line, line_em)
 
         stellar_nu[:] = 1.e8*constants.c.cgs.value/spec[0]
         stellar_fnu[i,:] = f
@@ -742,7 +746,7 @@ def agn_sed(agn_id, nu, fnu, metals_avg):
             line_em = line_lum * 3.839e33  # Units: ergs/s
             # The last column in dump_emlines is reserved for age of the star particle. For AGN we just set it to -1 as a place holder.
             line_em = np.append(line_em, -1.0)
-            dump_emlines(wave_line, line_em, id_val)
+            dump_emlines(wave_line, line_em)
 
     else:
         spec = fnu

powderday/nebular_emission/cloudy_tools.py
@@ -164,13 +164,15 @@ def get_nearest(particle_list, particle_central, num=32):
     return np.array(arg[0][mask]), np.array(arg[1][mask])
 
 
-def age_dist(Num, tavg, witdth=5, gamma=-0.65, bins=6, tries=100, tolerance = 0.1):
+def age_dist(Num, tavg, witdth=5, gamma=-0.65, bins=4, tries=100, tolerance = 0.1):
     # If number of star particles are less than 3 times the number of bins then do not
     # break them down since there are not enough particles to relaiably sample the
     # distribution
+    
+    tavg = tavg*1e3
 
-    #if Num < bins * 3:
-    #    return np.array([Num]), np.array([tavg])
+    if Num <= bins:
+        return np.array([Num]), np.array([tavg])
 
     # Solving for the limits of the age distribution (dN/dt is proportional to gamma) such that the
     # width (end - start age) is equal to the width parameter and the average age of the distribution is
@@ -181,7 +183,7 @@ def age_dist(Num, tavg, witdth=5, gamma=-0.65, bins=6, tries=100, tolerance = 0.
     # not met then the particle is not broken down
 
     for i in range(tries):
-        ti_temp = np.arange(1, cfg.par.HII_max_age, 0.1)
+        ti_temp = np.arange(1, cfg.par.HII_max_age*1e3, 0.1)
         x = (ti_temp + witdth) ** (gamma + 2) + ti_temp ** (gamma + 2) - 2 * (tavg ** (gamma + 2))
         index = np.argmin(np.abs(x))
         ti = ti_temp[index]
@@ -201,4 +203,4 @@ def age_dist(Num, tavg, witdth=5, gamma=-0.65, bins=6, tries=100, tolerance = 0.
         else:
             witdth = witdth - 0.2
 
-    return N, t
+    return N, t*1e-3