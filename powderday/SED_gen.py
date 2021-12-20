7feeab9f48cc9941d27385378998c46dc9805f57
Prerak Garg <prerakgarg07@gmail.com>
2021-12-19 19:45:46 -0500

    Updated docs and added backwards compatibility

--- powderday/SED_gen.py
+++ powderday/SED_gen.py
@@ -445,8 +445,7 @@ def newstars_gen(stars_list):
             if stars_list[i].mass / constants.M_sun.cgs.value > 10 ** cfg.par.cmdf_max_mass and cfg.par.use_cmdf:
                 cluster_mass, num_clusters = cmdf(stars_list[i].mass / constants.M_sun.cgs.value,
                                                   int(cfg.par.cmdf_bins), cfg.par.cmdf_min_mass,
-                                                  cfg.par.cmdf_max_mass, cfg.par.cmdf_beta,
-                                                  rescale_masses=cfg.par.cmdf_rescale)
+                                                  cfg.par.cmdf_max_mass, cfg.par.cmdf_beta)
                 age_clusters = []
                 for k in range(len(cluster_mass)):
                     age_clusters.append(stars_list[i].age)
